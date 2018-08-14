2ce6d548cd8fdd94baae32268c1c25413856035d
Jose Antonio <j-osea-nt@hotmail.com>
2018-08-13 21:33:11 -0500

    add current weather component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CurrentWeatherService } from './services/current-weather.service';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +8,9 @@ import { CurrentWeatherService } from './services/current-weather.service';
 export class AppComponent {
   title = 'app';
 
-  constructor(private weatherService : CurrentWeatherService){}
+  constructor(){}
 
   // se ejecuta cuando el componente esta listo
   ngOnInit(){
-    // this.weatherService.get();
-
-    // el servicio tiene un propiedad weather$ que es un observable q recibe los mismos datos q se reciben la peticion http del servicio (del observable q return http.get)
-    // lo q se logra es q ahora existe un propiedad en el componente al cual se puede suscribir, que este en una propidad va servir para trabajar con el template
-    this.weatherService.weather$.subscribe(console.log)
   }
 }
---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CurrentWeatherService } from '../services/current-weather.service';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.sass']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  // se hace public para ser accedido desde el template
+  constructor(public weatherService : CurrentWeatherService) { }
+
+  ngOnInit() {
+    // this.weatherService.get();
+    // el servicio tiene un propiedad weather$ que es un observable q recibe los mismos datos q se reciben la peticion http del servicio (del observable q return http.get)
+    // lo q se logra es q ahora existe un propiedad en el componente al cual se puede suscribir, que este en una propidad va servir para trabajar con el template
+    // this.weatherService.weather$.subscribe(console.log)
+  }
+
+}
