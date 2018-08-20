7e2ab020f62509fd5ea649e6630686f48debac93
Jose Antonio <j-osea-nt@hotmail.com>
2018-08-20 08:40:08 -0500

    structure data of forecast

--- src/app/services/forecast.service.ts
+++ src/app/services/forecast.service.ts
@@ -17,12 +17,46 @@ export class ForecastService {
 
   constructor(private http : HttpClient) {
     // obtener la version observable del sujeto
-    this.weather$ = this.weatherSubject.asObservable();
+    this.weather$ = this.weatherSubject.asObservable().pipe(map(this.structureData));
 
     this.get({
       lat: 35,
       lon: 139
     });
+  }
+
+  structureData(data : any){
+
+    // la clave sera mes - dia, se usa el mes pues si solo se usaría dia al pasar del 31 a 4,.. hubiese un problema para poder ver el mayor valor
+    let minMaxPerDay = {};
+
+    data.list.forEach(weatherObject => {
+      // para convertir una timestamp en una fecha que se pueda leer se multiplca por 1000 milisegundos
+      let date = new Date(weatherObject.dt * 1000)
+      let hours = date.getHours();
+      let month = date.getMonth();
+      let day = date.getDate();
+      let key = `${month}-${day}`;
+
+      // si ya tiene un valor en el objeto minMaxPerDay q use ese, sino q recien lo inicialice con {} (en caso de ser el primer valor del día)
+      let tempPerDay : Weather = minMaxPerDay[key] || {
+        minMaxTemp : {}
+      };
+
+      // el temp_min y temp_max de la prop main son valores que definen el rango de error para la temperatura
+      // al iniciarse el objeto tempPerDay, esta vacio por tanto si no tiene valores se le asigna la temp_min, o si tiene valor y la temp_min es menor a la temp_min hasta el momento, se le asigna temp_min
+      if(!tempPerDay.minMaxTemp.min || (weatherObject.main.temp_min < tempPerDay.minMaxTemp.min)){
+        tempPerDay.minMaxTemp.min = weatherObject.main.temp_min;
+      }
+      
+      if(!tempPerDay.minMaxTemp.max || (weatherObject.main.temp_max > tempPerDay.minMaxTemp.max)){
+        tempPerDay.minMaxTemp.max = weatherObject.main.temp_max;
+      }
+
+      minMaxPerDay[key] = tempPerDay;
+    });
+
+    return minMaxPerDay;
   }
 
   get(coords : Coords){
--- src/structures/weather.structure.ts
+++ src/structures/weather.structure.ts
@@ -1,8 +1,19 @@
 export interface Weather{
-  name : string;
-  cod : number;
-  icon : string;
-  description : string;
-  temp : number;
-  main : string;
+  name ?: string;
+  cod ?: number;
+  icon ?: string;
+  description ?: string;
+  temp ?: number;
+  main ?: string;
+  minMaxTemp ?: MinMaxInterface;
+}
+
+// el date es el numero del dia en el mes
+// day es el numero en la semana (1-6)
+interface MinMaxInterface{
+  date ?: number;
+  day ?:number;
+  month ?:number;
+  min ?:number;
+  max ?:number;s
 }
